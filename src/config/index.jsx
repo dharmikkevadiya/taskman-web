4296171372a862c6855eee4dd187dbb1233f2f18
Dharmik Kevadiya <dharmikkevadiya1552@gmail.com>
2023-07-11 18:31:47 +0530

    change base url

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,4 +1,4 @@
 // const BASE_URL = "http://localhost:5000";
-const BASE_URL = "https://taskman-server.onrender.com";
+const BASE_URL = "https://taskman-server-1qgsrq4sj-dharmikkevadiya.vercel.app";
 
 export { BASE_URL };
